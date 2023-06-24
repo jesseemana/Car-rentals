f5d60491c0feb0d0cc21dd02744bddd506bb3d9b
Jesse Emana <jesseemana@gmail.com>
2023-06-24 22:29:51 +0200

    navbar and footer created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar'
 import './globals.css'
 
 export const metadata = {
@@ -9,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className='relative'>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
