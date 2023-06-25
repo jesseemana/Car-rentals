d55bf0dcbf5cae9471bdcd5fd101c83d5a1b88f3
Jesse Emana <jesseemana@gmail.com>
2023-06-25 14:12:43 +0200

    modified code line spacing

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -17,22 +17,22 @@ interface ButtonProps {
 const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: ButtonProps) => {
   return (
     <button
-        disabled={isDisabled}
-        type={btnType || 'button'}
-        className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none ${containerStyles}`}
-        onClick={handleClick}
+      disabled={isDisabled}
+      type={btnType || 'button'}
+      className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none ${containerStyles}`}
+      onClick={handleClick}
     >
-        <span className={`flex-1 ${textStyles}`}>{title}</span>
-        {rightIcon && (
-            <div className='relative w-6 h-6'>
-                <Image 
-                    src={rightIcon}
-                    alt='arrow_left'
-                    fill
-                    className='object-contain'
-                />
-            </div>
-        )}
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image 
+            src={rightIcon}
+            alt='arrow_left'
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
     </button>
   )
 }
