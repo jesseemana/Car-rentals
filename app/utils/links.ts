ba4b791487ce140a714f675d4f4b5267088cb78b
Jesse Emana <jesseemana@gmail.com>
2023-10-09 08:44:13 +0200

    created favicon and exported some data

---
+++ app/utils/links.ts
@@ -0,0 +1,28 @@
+export const footer_links = [
+  {
+    title: 'About',
+    links: [
+      { title: 'How it works', url: '/' },
+      { title: 'Featured', url: '/' },
+      { title: 'Partnership', url: '/' },
+      { title: 'Bussiness Relation', url: '/' },
+    ],
+  },
+  {
+    title: 'Company',
+    links: [
+      { title: 'Events', url: '/' },
+      { title: 'Blog', url: '/' },
+      { title: 'Podcast', url: '/' },
+      { title: 'Invite a friend', url: '/' },
+    ],
+  },
+  {
+    title: 'Socials',
+    links: [
+      { title: 'Instagram', url: 'instagram.com' },
+      { title: 'Twitter', url: 'twiter.com/jesseemana' },
+      { title: 'Facebook', url: 'facebook.com' },
+    ],
+  },
+];  
