9641d80744c594a8d5be452ce03d9200bd526b3a
Jesse Emana <jesseemana@gmail.com>
2023-10-10 21:27:51 +0200

    added routing to home page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,19 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center px-[4%] shadow-sm py-3'>
-      <Image
-        src='/logo.png' 
-        alt='website logo'
-        width={80}
-        height={80}
-      />
-      <div className='hidden flex gap-5 capitalize font-normal text-gray-600 lg:flex hover:cursor-pointer'>
-        <h1>home</h1>
+      <Link href='/'>
+        <Image
+          src='/logo.png' 
+          alt='website logo'
+          width={80}
+          height={80}
+        />
+      </Link>
+      <div className='hidden  gap-5 capitalize font-normal text-gray-600 lg:flex hover:cursor-pointer'>
+        <Link href='/'>home</Link>
         <h1>about</h1>
         <h1>services</h1>
         <h1>contact</h1>
