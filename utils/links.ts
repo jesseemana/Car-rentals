5b6f19d6c3429b4a6c928cdd754f9a0ef440e65a
Jesse Emana <jesseemana@gmail.com>
2023-10-11 21:36:01 +0200

    moved utils folder

---
+++ utils/links.ts
@@ -0,0 +1,11 @@
+export const footer_links = [
+  {
+    title: 'About',
+    links: [
+      { title: 'How it works', url: '/' },
+      { title: 'Featured', url: '/' },
+      { title: 'Partnership', url: '/' },
+      { title: 'Bussiness Relation', url: '/' },
+    ],
+  }
+];  
